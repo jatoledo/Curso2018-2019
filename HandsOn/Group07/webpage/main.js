f8f309fd94e5d816b2552d5ae2dfb12cc9a5ffd9
lucianogg <lucgarc97@hotmail.com>
2018-12-06 11:23:30 +0100

    main.js is capable of reaching to Wikidata and fetch data from the neighbourhood

---
+++ HandsOn/Group07/webpage/main.js
@@ -0,0 +1,41 @@
+var data = [];
+data.push(
+    {
+        neighbourhood: "Q1763694"
+    }
+);
+
+var neighbourhoodDataQuery = [
+    "SELECT distinct ?neighbourhoodcode ?name ?population ?area ?imageURI",
+    "WHERE {",
+        "?neighbourhoodcode wdt:P31 wd:Q3032114.",
+        "?neighbourhoodcode wdt:P1082 ?population.",
+        "?neighbourhoodcode wdt:P2046 ?area.",
+        "?neighbourhoodcode rdfs:label ?name.",
+        "FILTER (lang(?name) in ('es')).",
+        "OPTIONAL { ?neighbourhoodcode wdt:P18 ?imageURI. }",
+    "}"
+].join(" ");
+
+$.ajax({
+    url: "https://query.wikidata.org/sparql?query=" + encodeURIComponent(neighbourhoodDataQuery) + "&format=json",
+    success: function(_data) {
+        console.log(_data.results.bindings);
+        _data.results.bindings.forEach((wdNeigh) => {
+            data.forEach((neighbourhood) => {
+                if ("http://www.wikidata.org/entity/" + neighbourhood.neighbourhood == wdNeigh.neighbourhoodcode.value) {
+                    neighbourhood.neighbourhoodInfo = {};
+                    neighbourhood.neighbourhoodInfo.population = parseFloat(wdNeigh.population.value);
+                    neighbourhood.neighbourhoodInfo.area = parseFloat(wdNeigh.area.value);
+                    neighbourhood.neighbourhoodInfo.name = wdNeigh.name.value;
+                    neighbourhood.neighbourhoodInfo.imageURI = wdNeigh.imageURI.value;
+                }
+            });
+        });
+    },
+    async: false
+});
+
+data.forEach((neighbourhood) => {
+    
+})
